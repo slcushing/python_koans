cd9f6ac9e85a39de01fe3f53c09e1d45e37d7764
Stephanie Cushing <slcushing@gmail.com>
2021-10-06 14:32:31 -0400

    Sets, True&False, Triangle1 complete

--- koans/triangle.py
+++ koans/triangle.py
@@ -17,9 +17,15 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    # DELETE 'PASS' AND WRITE THIS CODE
-    pass
-
+    if a <= 0 or b <= 0 or c <= 0: #triangle can't have a negative side
+        raise TriangleError(AttributeError('Negative number not allowed'))
+    if a + b + c <= 2 * max(a,b,c):
+        raise TriangleError(AttributeError('largest side should be samller than the sum of the other two sides'))
+    if a == b == c:
+        return 'equilateral'
+    elif a == b or b == c or c == a:
+        return 'isosceles'
+    return 'scalene'
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
     pass
